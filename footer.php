19be46ad321deef9c6fcd30583c1eca4e6aafc1a
Simon Scarfe <simon.scarfe@gmail.com>
2012-03-06 00:12:11 +0000

    initial mmr work

---
+++ footer.php
@@ -0,0 +1,6 @@
+            </div> <!-- /.main-column -->
+            <div class="birds"></div> <!-- /.birds -->
+        </div> <!-- /#main -->
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></head>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="main">
+        <a href="/" id="logo">Middlesbrough Milk Rollers</a>
+        <div class="bnr">
+            banner
+        </div>
+        <div class="left-column">
+            left col
+        </div>
+        <div class="main-column">
