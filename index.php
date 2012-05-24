35f8ef258090859bafe74fd9fa20e389a84ddbd9
Simon Scarfe <simon.scarfe@gmail.com>
2012-05-24 21:38:41 +0100

    blog index page styling

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
 	yoast_breadcrumb('<p id="breadcrumbs">','</p>');
 } ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
+<h1>Blog</h1>
+<div class="list">
+    <?php while ( have_posts() ) : the_post(); ?>
 
-<div <?php post_class(); ?>>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> 
-    <p class="date"><?php the_date(); ?></p>
-    <?php the_content(); ?>
-</div>
+    <div <?php post_class(); ?>>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="date"><?php the_date(); ?></span></h2> 
+        
+        <?php the_content(); ?>
+    </div>
 
-<?php endwhile; ?>
+    <?php endwhile; ?>
+</div> <!-- /.list -->
 
 
 <?php get_footer(); ?>
