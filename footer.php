52efd2b3c553b7c20d1d0779a6c2fb3ba4845747
Simon Scarfe <simon.scarfe@gmail.com>
2012-06-26 23:21:26 +0100

    removed sponsors

--- footer.php
+++ footer.php
@@ -1,13 +1,4 @@
             </div> <!-- /.main-column -->
-            <div class="footer">
-                <div class="icons">
-                    <a href="#" class="tft">Ten Feet Tall</a>
-                    <a href="#" class="wicked">Wicked Skatewear</a>
-                    <a href="#" class="popcorn">Popcorn Skate</a>
-                    <a href="http://rollergirl.ca" target="_blank" class="rollergirl">rollergirl.ca</a>
-                    <a href="#" class="greendragon">Ten Feet Tall</a>
-                </div>
-            </div>
             <div class="birds"></div> <!-- /.birds -->
         </div> <!-- /#main -->
         <?php wp_footer(); ?>
